cf92d8323da03a9bb91d08ece735a05e41ea02ca
Alex Sosnovskiy <devbyru@gmail.com>
2025-06-29 12:53:27 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vite';
-import gltf from 'vite-plugin-gltf';
-import { draco, resample, prune } from '@gltf-transform/functions';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    gltf({
-      transforms: [resample(), prune(), draco()],
-    }),
-  ],
+  plugins: [],
   build: {
-    target: 'es6',
+    target: 'es2022',
     assetsInlineLimit: 0, //disable
-    cssTarget: 'chrome61',
+    cssTarget: 'chrome100',
   },
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
     APP_STAGE: JSON.stringify(process.env.APP_ENV || 'unk'),
     QA_ENABLED: JSON.stringify(process.env.QA ? 'true' : ''),
-    'import.meta.env.QA_ENABLED': JSON.stringify(process.env.QA ? 'TEST MODE ENABLED' : ''),
+    'import.meta.env.QA_ENABLED': JSON.stringify(
+      process.env.QA ? 'TEST MODE ENABLED' : ''
+    ),
   },
 });
